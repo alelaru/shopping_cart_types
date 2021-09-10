81ab4d807705b7da2223dc0c13e12aa7a5ff186e
Alejandro Lara <alelaru@gmail.com>
2021-09-10 18:33:19 +0200

    Shop items obtained from fakestoreapi, logged the info in console and have a loading and error handling for the API request

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { LinearProgress } from "@material-ui/core";
+import { useQuery } from "react-query";
+
+
+export type CartItemType = {
+  id: number,
+  category: string, 
+  description: string,
+  price: number,
+  image: string,
+  title: string,
+  amount: number
+}
+
+const getProducts = async (): Promise<CartItemType[]> => 
+   await(await fetch("https://fakestoreapi.com/products")).json();
 
 function App() {
+
+  const {data, isLoading, error} = useQuery<CartItemType[]>(
+    'products', 
+    getProducts
+  )
+  
+  console.log(data);
+  
+  const getTotalItems = () => null;
+
+  const handleAddToCart = () => null;
+
+  const removeFromCart = () => null;
+
+  if(isLoading) return <LinearProgress/>
+  if(error) return <div>Something went wrong</div>
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Start
     </div>
   );
 }
 
 export default App;
+
+
+////https://www.youtube.com/watch?v=sfmL6bGbiN8&ab_channel=freeCodeCamp.org
